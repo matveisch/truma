b766f27949c99eab4f946dc5c8b7a923d2a26313
Matvei <matvesch@protonmail.com>
2023-10-10 23:34:40 +0300

    Add toggle component and update application interface
    Adding a new toggle component to the user interface, alongside updates to the main application interface. The changes include the addition of a @radix-ui/react-toggle dependency, as this library is necessary to make the toggle component work as intended. Additional UI components have also been added into the main page interface, along with some array objects representing filters to demonstrate their usage in the application. These additions and adjustments contribute to enhancing the interactivity and user experience of the application.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
 import Header from '@/components/Header';
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Toggle } from '@/components/ui/toggle';
 
 export default function Home() {
+  const filters = [
+    { name: 'דיור', options: ['בן אדם 1', '1-3 אנשים', '3-10 אנשים', 'יותר מ-10 אנשים'] },
+    { name: 'אוכל', options: [] },
+    { name: 'בעלי חיים', options: [] },
+    { name: 'ילדים', options: [] },
+    { name: 'עזרה רוחנית', options: [] },
+    { name: 'תובלה', options: [] },
+    { name: 'בגדים', options: [] },
+    { name: 'אחר', options: [] },
+  ];
+
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <Header />
-      <Tabs defaultValue="need-help" className="w-[250px]">
+      <Tabs defaultValue="need-help" className="w-[250px] mt-10">
         <TabsList className="w-full">
           <TabsTrigger value="offer-help" className="w-full">
             מציע עזרה
@@ -15,6 +27,24 @@ export default function Home() {
           </TabsTrigger>
         </TabsList>
       </Tabs>
+      <div className="flex gap-5 w-full mt-[60px]">
+        {filters.map((filter, index) => (
+          <Toggle key={`${filter.name}-${index}`} variant="outline">
+            {filter.name}
+          </Toggle>
+        ))}
+      </div>
+      <div className="w-full mt-5">
+        <Tabs defaultValue="need-help" className="w-fit">
+          <TabsList className="w-full rtl:flex-row-reverse flex">
+            {filters[0].options.map((option, index) => (
+              <TabsTrigger value={option} className="w-full" key={`${option}-${index}`}>
+                {option}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+        </Tabs>
+      </div>
     </main>
   );
 }
