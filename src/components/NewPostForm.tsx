c61ec9de637f35203acc23dc4a91d47b8fe64b67
Matvei <matvesch@protonmail.com>
2023-10-11 23:36:32 +0300

    Add Importance Toggle to NewPostForm
    Added a new "importance" field to the NewPostForm component. This allows users to set the urgency level of a post when creating a new one. A Toggle UI component was implemented for user to select one of the predefined time options. The selected option is stored in the "importance" state using the 'useEffect' hook, which monitors any changes in the activeToggle state and updates the 'importance' field value.

--- src/components/NewPostForm.tsx
+++ src/components/NewPostForm.tsx
@@ -15,6 +15,8 @@ import {
 import { Input } from '@/components/ui/input';
 import { ComboBox } from '@/components/ComboBox';
 import { Textarea } from '@/components/ui/textarea';
+import { Toggle } from '@/components/ui/toggle';
+import { useEffect, useState } from 'react';
 
 export const formSchema = z.object({
   name: z.string().min(1, {
@@ -27,18 +29,22 @@ export const formSchema = z.object({
     message: 'Must be filled',
   }),
   description: z.string(),
+  importance: z.string(),
 });
 
 export default function NewPostForm() {
-  const { setValue, ...form } = useForm<z.infer<typeof formSchema>>({
+  const { getValues, setValue, ...form } = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: '',
       city: '',
       phone: '',
       description: '',
+      importance: '',
     },
   });
+  const timeOptions = ['שעה 1', '12 שעות', '24 שעות', 'לא דחוף'];
+  const [activeToggle, setActiveToggle] = useState<string>('לא דחוף');
 
   function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
@@ -46,8 +52,12 @@ export default function NewPostForm() {
     console.log(values);
   }
 
+  useEffect(() => {
+    setValue('importance', activeToggle);
+  }, [activeToggle]);
+
   return (
-    <Form {...form} setValue={setValue}>
+    <Form {...form} setValue={setValue} getValues={getValues}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="flex items-start gap-20 flex-wrap">
         <div>
           <FormField
@@ -93,6 +103,21 @@ export default function NewPostForm() {
           </div>
         </div>
         <div className="flex flex-col items-start justify-start">
+          <div className="flex flex-col gap-3 w-full">
+            <FormLabel>דחפות</FormLabel>
+            <div className="flex gap-2">
+              {timeOptions.map((option, index) => (
+                <Toggle
+                  pressed={activeToggle === option}
+                  key={`${option}-${index}`}
+                  variant="outline"
+                  onClick={() => setActiveToggle(option)}
+                >
+                  {option}
+                </Toggle>
+              ))}
+            </div>
+          </div>
           <FormField
             control={form.control}
             name="description"
