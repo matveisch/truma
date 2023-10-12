66857f379cd08fd57987c456d1b5d439d15b51b6
Matvei <matvesch@protonmail.com>
2023-10-12 18:27:19 +0300

    Add Menubar component and dynamic responsiveness in Header
    Implemented a new Menubar component to support site navigability. This was added using the react-menubar library to create an interactive menubar within the Header component. This menubar is responsive, adapting to the screen size. This change improves user navigation experience and site flexibility across devices. Made corresponded library updates in package.json and package-lock.json files. Introduced changes in the Header component to dynamically render elements depending on the screen size, providing an enhanced user interface experience.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,55 @@
-import Image from 'next/image';
-import globe from '@/public/Material Symbols Language.svg';
 import { Button } from '@/components/ui/button';
 import { ReactNode } from 'react';
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from '@/components/ui/menubar';
+import { Globe, Menu } from 'lucide-react';
 
 export default function Header({ children }: { children: ReactNode }) {
   return (
-    <header className="flex justify-between items-center w-full relative rtl:flex-row-reverse">
-      <div className="flex gap-1 ">
-        <Image src={globe} alt={'globe icon'} />
-        עברית
+    <header className="w-full">
+      <div className="invisible sm:visible flex justify-between items-center w-full relative rtl:flex-row-reverse">
+        <div className="flex gap-1 ">
+          <Globe />
+          עברית
+        </div>
+        <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          מיד ליד
+        </h1>
+        <div className="flex gap-6">
+          {children}
+          <Button variant="ghost">ארגוני מתנדבים</Button>
+          <Button variant="ghost">מידע</Button>
+        </div>
       </div>
-      <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        מיד ליד
-      </h1>
-      <div className="flex gap-6">
-        {children}
-        <Button variant="ghost">ארגוני מתנדבים</Button>
-        <Button variant="ghost">מידע</Button>
+      <div className="flex justify-between w-full items-center sm:invisible">
+        <Menubar>
+          <MenubarMenu>
+            <MenubarTrigger>
+              <Menu />
+            </MenubarTrigger>
+            {/*todo: make direction dynamic*/}
+            <MenubarContent style={{ direction: 'rtl' }}>
+              <MenubarItem>
+                <Button variant="ghost">ארגוני מתנדבים</Button>
+              </MenubarItem>
+              <MenubarItem>
+                <Button variant="ghost">מידע</Button>
+              </MenubarItem>
+              <MenubarItem>
+                <Button variant="ghost" className="flex gap-1">
+                  <Globe />
+                  עברית
+                </Button>
+              </MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+        </Menubar>
+        <h1 className="">מיד ליד</h1>
       </div>
     </header>
   );
