2e2be8c814aec03cb0ab57adfdd0792d4406ebb2
Matvei <matvesch@protonmail.com>
2023-10-14 23:38:25 +0300

    Add Privacy Policy page and update package dependencies.
    Created a new Privacy Policy page component to improve transparency on data collection and usage. Also updated package-lock.json as a result of adding packages 'remark' and 'remark-html' for markdown support. The Privacy Policy page ensures compliance with data protection laws and provides clarity to users about how their information is used. The additional packages enhance our markdown rendering capabilities, allowing for more flexibility in content styling and presentation.

---
+++ src/app/[lang]/privacy/page.tsx
@@ -0,0 +1,46 @@
+export default function Home() {
+  return (
+    <div className="p-4 max-w-lg mx-auto">
+      <h1 className="font-bold text-2xl mb-4">Privacy Policy for Meyad-Leyad</h1>
+      <p className="mb-2">
+        <strong className="font-semibold">1. Information We Collect</strong> - Meyad-Leyad may
+        collect the following information from users: - Full name - Phone number - Name of the part
+        of the country the user lives in
+      </p>
+      <p className="mb-2">
+        <strong className="font-semibold">2. Use of Information</strong> - The information collected
+        is used solely for the purpose of facilitating connections between users seeking help and
+        support on Meyad-Leyad. - We do not share this information with third parties or use it for
+        marketing purposes.
+      </p>
+      <p className="mb-2">
+        <strong className="font-semibold">3. Protection of Information</strong> - We implement
+        reasonable security measures to protect the information you provide. However, no data
+        transmission over the internet can be guaranteed as 100% secure.
+      </p>
+      <p className="mb-2">
+        <strong className="font-semibold">4. Cookies</strong> - Meyad-Leyad may use cookies to
+        enhance user experience. Users can disable cookies through their web browser settings if
+        they prefer not to allow them.
+      </p>
+      <p className="mb-2">
+        <strong className="font-semibold">5. Third-Party Links</strong> - Our website may contain
+        links to third-party websites. We are not responsible for the privacy practices of those
+        websites.
+      </p>
+      <p className="mb-2">
+        <strong className="font-semibold">6. Children's Privacy</strong> - Meyad-Leyad is intended
+        for use by adults, and we do not knowingly collect or store information from children under
+        the age of 13.
+      </p>
+      <p className="mb-2">
+        <strong className="font-semibold">7. Changes to Privacy Policy</strong> - We may update this
+        privacy policy as needed. Users will be notified of any changes.
+      </p>
+      <p className="mb-4">
+        <strong className="font-semibold">8. Contact Information</strong> - If you have questions or
+        concerns about our privacy policy, please contact us at support@desight.co.
+      </p>
+    </div>
+  );
+}
