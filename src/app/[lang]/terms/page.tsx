534168704f64765e59ec71bad9883a6069662da0
Matvei <matvesch@protonmail.com>
2023-10-14 23:46:01 +0300

    Add Terms of Use page for Meyad-Leyad website
    Implemented a new Terms of Use page in Meyad-Leyad website to provide clear guidelines on the acceptable use of the website. These terms explain users rights and responsibilities, content ownership, user conduct, privacy aspects, termination procedures and other relevant issues. Adding Terms of Use page is a significant step to ensure legal compliance and protection from potential liabilities while also fostering trust among users.

---
+++ src/app/[lang]/terms/page.tsx
@@ -0,0 +1,58 @@
+export default function Home() {
+  return (
+    <div className="p-4 max-w-lg mx-auto text-left">
+      <h1 className="font-bold text-2xl mb-4">Terms of Use for Meyad-Leyad</h1>
+      <p className="mb-2">
+        <strong className="font-semibold">1. Acceptance of Terms</strong> By using Meyad-Leyad, you
+        agree to abide by these Terms of Use. If you do not agree with any part of these terms,
+        please refrain from using the website.
+      </p>
+      <p className="mb-2">
+        <strong className="font-semibold">2. User Conduct</strong> - You agree to use the website
+        for its intended purpose, which is to connect with others for help and support. - You will
+        not engage in any unlawful, abusive, or harmful activities while using the website. - You
+        are responsible for the content you post on the website, and it should not violate any laws
+        or infringe on the rights of others.
+      </p>
+      <p className="mb-2">
+        <strong className="font-semibold">3. Content Ownership</strong> - Users retain ownership of
+        the content they post on the website. - By posting content, you grant Meyad-Leyad a
+        non-exclusive, royalty-free license to use, modify, and distribute your content for the
+        purpose of operating the website.
+      </p>
+      <p className="mb-2">
+        <strong className="font-semibold">4. Privacy</strong> - We respect your privacy. Please
+        refer to our Privacy Policy [Insert link to Privacy Policy] to understand how we collect,
+        use, and protect your personal information.
+      </p>
+      <p className="mb-2">
+        <strong className="font-semibold">5. Termination</strong> - We reserve the right to
+        terminate or suspend your access to the website if you violate these terms or engage in any
+        harmful activities.
+      </p>
+      <p className="mb-2">
+        <strong className="font-semibold">6. Disclaimer</strong> - Meyad-Leyad provides its services
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
+        "as is." We make no warranties or guarantees regarding the accuracy, availability, or
+        reliability of the website.
+      </p>
+      <p className="mb-2">
+        <strong className="font-semibold">7. Limitation of Liability</strong> - We will not be
+        liable for any direct, indirect, or incidental damages arising from the use of this website.
+      </p>
+      <p className="mb-2">
+        <strong className="font-semibold">8. Changes to Terms</strong> - We may update these terms
+        from time to time. You will be notified of any changes, and it's your responsibility to
+        review the updated terms.
+      </p>
+      <p className="mb-2">
+        <strong className="font-semibold">9. Governing Law</strong> - These terms are governed by
+        the laws of Israel.
+      </p>
+      <p className="mb-4">
+        <strong className="font-semibold">10. Contact</strong> - If you have any questions or
+        concerns regarding these terms, please contact us at support@desight.co.
+      </p>
+    </div>
+  );
+}
