ddd30839c9763a3f17082dd6ac759bb35f836fb2
Tommy Lidkovsky <armkudr@gmail.com>
2023-11-06 12:43:03 +0200

    Seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,57 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Params } from 'next/dist/shared/lib/router/utils/route-matcher';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: 'מיד ליד',
-  description:
-    'אתר הפרסום המוביל למציאת עזרה והצפת מודעות. זקוק לעזרה? רוצה לסייע? הצטרף אלינו ותרום לקהילה. מודעות עזרה, פרסום בחינם',
-};
+// export const metadata: Metadata = {
+//   title: 'מיד ליד',
+//   description:
+//     'אתר הפרסום המוביל למציאת עזרה והצפת מודעות. זקוק לעזרה? רוצה לסייע? הצטרף אלינו ותרום לקהילה. מודעות עזרה, פרסום בחינם',
+// };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export async function generateMetadata({ params }: { params: Params }): Promise<Metadata> {
+  const russianMetadata: Metadata = {
+    title: 'Платформа помощи и волонтерства מיד ליד: объявления о помощи бесплатно',
+    description:
+      'Присоединяйтесь к платформе מיד ליד для размещения и поиска объявлений о помощи и волонтерской деятельности. У нас вы найдете волонтерские возможности и поддержку бесплатно.',
+  };
+
+  const englishMetadata: Metadata = {
+    title:
+      'מיד ליד - Volunteering and Assistance Platform in Israel | Post and Offer Help for Free',
+    description:
+      'מיד ליד is a volunteering and assistance platform in Israel. Post or join to find help within our community. Everything is free and in the spirit of volunteering.',
+  };
+
+  const hebrewMetadata: Metadata = {
+    title: 'מיד ליד - פלטפורמת התנדבות ועזרה בישראל | פרסום ומציעים עזרה חינם',
+    description:
+      'מיד ליד היא פלטפורמת התנדבות ועזרה בישראל. פרסמו או הצטרפו למציאת עזרה במדינה שלנו. הכל בחינם וברוח התנדבותית.',
+  };
+
+  if (params.lang === 'en') {
+    return englishMetadata;
+  } else if (params.lang === 'ru') {
+    return russianMetadata;
+  } else {
+    return hebrewMetadata;
+  }
+}
+export async function generateStaticParams() {
+  return [{ lang: 'en' }, { lang: 'he' }, { lang: 'ru' }];
+}
+export default function RootLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { lang: string };
+}) {
+  console.log(params.lang + ' PARANS');
   return (
-    <html>
+    <html lang={params.lang}>
       <body className={`relative min-h-[calc(100vh-48px)] ${inter.className}`}>
         {children}
         <footer className="w-full bg-slate-100 py-3 absolute bottom-[-48px]">
--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useEffect, useState, useRef } from 'react';
 import { createClient } from '@supabase/supabase-js';
 import { Database } from '@/lib/supabase';
 import Header from '@/components/Header';
@@ -11,7 +11,8 @@ import Filters from '@/components/Filters';
 import Posts from '@/components/Posts';
 import { Toaster } from '@/components/ui/toaster';
 import NewPostForm from '@/components/NewPostForm';
-
+import Image from 'next/image';
+import heroImage from '@/public/hero_img.svg';
 export interface ContextType {
   dict: any;
   lang: 'en' | 'ru' | 'he';
@@ -25,10 +26,17 @@ export default function MainPage({ dict, lang }: { dict: any; lang: 'en' | 'ru' 
   const [createMode, setCreateMode] = useState(false);
   const [needHelp, setNeedHelp] = useState(true);
   const [selectedArea, setSelectedArea] = useState('');
-
+  // const topRef = useRef<HTMLDivElement>(null);
+  // const [marginTop, setMarginTop] = useState<number>();
   const supabaseUrl = 'https://eszdtlbcthjrkryjrlaa.supabase.co';
   const supabaseKey = process.env.SUPABASE_KEY;
   const supabase = createClient<Database>(supabaseUrl, supabaseKey || '');
+  // useEffect(() => {
+  //   let margin =
+  //     topRef.current?.offsetHeight != undefined ? topRef.current?.offsetHeight + 20 : 100;
+  //   setMarginTop(margin);
+  //   console.log(marginTop);
+  // }, [topRef.current?.offsetHeight]);
 
   useEffect(() => {
     document.documentElement.lang = lang;
@@ -46,71 +54,102 @@ export default function MainPage({ dict, lang }: { dict: any; lang: 'en' | 'ru' 
 
   return (
     <main
-      className="flex min-h-screen flex-col items-center max-w-[1280px] m-auto sm:p-10 p-3"
+      className="max-w-[1280px] m-auto"
       style={lang === 'he' ? { direction: 'rtl' } : undefined}
     >
-      <Context.Provider value={{ dict, lang }}>
-        <Header>
-          <Button
-            className="flex gap-1 items-center rtl:flex-row-reverse"
-            onClick={() => setCreateMode(!createMode)}
-          >
-            {!createMode && (
-              <>
-                <Plus />
-                {dict.header.newAd}
-              </>
-            )}
-            {createMode && (
-              <>
-                <HomeIcon />
-              </>
-            )}
-          </Button>
-        </Header>
-        <TabSwitcher setNeedHelp={setNeedHelp} />
-        {!createMode && (
-          <>
-            <Filters
-              setSelectedArea={setSelectedArea}
-              activeToggle={activeToggle}
-              setActiveToggle={setActiveToggle}
-              activeOption={activeOption}
-              setActiveOption={setActiveOption}
-            />
-            <Posts
-              selectedArea={selectedArea}
-              supabase={supabase}
-              needHelp={needHelp}
-              activeOption={activeOption}
-              activeToggle={activeToggle}
-            />
-          </>
-        )}
-
-        {createMode && (
-          <>
-            <div className="flex justify-start w-full">
-              <p className="text-3xl">{needHelp ? dict.form.title : dict.form.toHelpTitle}</p>
+      <div className="bg-white z-10 relative flex flex-col items-center sm:p-10 p-3 m-auto min-h-screen ">
+        <Context.Provider value={{ dict, lang }}>
+          <Header>
+            <Button
+              className="flex gap-1 items-center rtl:flex-row-reverse"
+              onClick={() => setCreateMode(!createMode)}
+            >
+              {!createMode && (
+                <>
+                  <Plus />
+                  {dict.header.newAd}
+                </>
+              )}
+              {createMode && (
+                <>
+                  <HomeIcon />
+                </>
+              )}
+            </Button>
+          </Header>
+          <div className="mt-6 flex border-8 p-4 border-blue-50 rounded-md flex-col md:flex-row">
+            <div className="bg-blue-50 rounded-md p-6  flex items-center justify-center flex-col gap-4 w-full">
+              <p className=" text-center text-lg md:text-start">{dict.seo.text1}</p>
+              {/* <p>
+                Наш ресурс предоставляет платформу для тех, кто готов оказать поддержку и для тех,
+                кто ищет содействие в различных сферах жизни. Независимо от того, нужна вам
+                поддержка с жильем, транспортом, питанием, одеждой или чем-либо еще, у нас вы
+                найдете ответы на свои потребности
+              </p> */}
+              {/* <p>
+                Что делает нашу платформу особенной - это возможность создать объявление абсолютно
+                бесплатно. Вы можете разместить свою просьбу или предложение о содействии, чтобы
+                найти тех, кто готов прийти на помощь в трудную минуту.
+              </p>
+              <p>
+                Присоединяйтесь к מיד ליד и используйте нашу платформу для того, чтобы делать добро
+                и находить поддержку в своем сообществе. Вместе мы можем сделать мир лучше.
+              </p> */}
             </div>
-            <Filters
-              createMode={createMode}
-              activeToggle={activeToggle}
-              setActiveToggle={setActiveToggle}
-              activeOption={activeOption}
-              setActiveOption={setActiveOption}
-            />
-            <NewPostForm
-              setCreateMode={setCreateMode}
-              needHelp={needHelp}
-              activeOption={activeOption}
-              activeFilter={activeToggle}
-              supabase={supabase}
-            />
-          </>
-        )}
-        <Toaster />
-      </Context.Provider>
+            <div className="w-full flex items-center justify-center">
+              <Image src={heroImage} alt="support help" className="h-60 w-auto" />
+            </div>
+          </div>
+
+          <TabSwitcher setNeedHelp={setNeedHelp} />
+          {!createMode && (
+            <>
+              <Filters
+                setSelectedArea={setSelectedArea}
+                activeToggle={activeToggle}
+                setActiveToggle={setActiveToggle}
+                activeOption={activeOption}
+                setActiveOption={setActiveOption}
+              />
+              <Posts
+                selectedArea={selectedArea}
+                supabase={supabase}
+                needHelp={needHelp}
+                activeOption={activeOption}
+                activeToggle={activeToggle}
+              />
+            </>
+          )}
+
+          {createMode && (
+            <>
+              <div className="flex justify-start w-full">
+                <p className="text-3xl">{needHelp ? dict.form.title : dict.form.toHelpTitle}</p>
+              </div>
+              <Filters
+                createMode={createMode}
+                activeToggle={activeToggle}
+                setActiveToggle={setActiveToggle}
+                activeOption={activeOption}
+                setActiveOption={setActiveOption}
+              />
+              <NewPostForm
+                setCreateMode={setCreateMode}
+                needHelp={needHelp}
+                activeOption={activeOption}
+                activeFilter={activeToggle}
+                supabase={supabase}
+              />
+            </>
+          )}
+          <Toaster />
+        </Context.Provider>
+        <div className="p-6 flex flex-col gap-4 bg-blue-50 rounded-md">
+          <p>{dict.seo.text2}</p>
+          <p>{dict.seo.text3}</p>
+          <p>{dict.seo.text4}</p>
+        </div>
+      </div>
     </main>
   );
 }
